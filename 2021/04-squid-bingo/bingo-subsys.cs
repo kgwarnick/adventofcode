aa17f3b9b41a5cefcb0307b6a8cfc8bbb6ebbd99
kgwarnick <118844595+kgwarnick@users.noreply.github.com>
2022-11-14 17:29:11 +0100

    AoC 2021, Day 4, Teil 2 Last Winner

--- 2021/04-squid-bingo/bingo-subsys.cs
+++ 2021/04-squid-bingo/bingo-subsys.cs
@@ -55,14 +55,21 @@ public class BingoSubsystem {
         return true;   // Setup successful
     }
 
+    /// <summary>Draw the numbers until everyone is finished</summary>
     public static void Run(List<int> numbers, List<BingoBoard> bblist) {
         Console.WriteLine($"Drawing at most {numbers.Count} numbers for {bblist.Count} boards");
         int lastNumber;
-        List<BingoBoard> winnerBoards = Play(bblist, numbers, out lastNumber);
+        List<BingoBoard> finalBoards = new List<BingoBoard>();
+        int finalNumber;
+        List<BingoBoard> winnerBoards = Play(bblist, numbers, out lastNumber, out finalNumber, out finalBoards);
         Console.WriteLine($"Last drawn number: {lastNumber}, winners: {winnerBoards.Count}");
         foreach (BingoBoard winner in winnerBoards) {
             int summeFrei = winner.GetUnmarked().Aggregate((sum, n) => n + sum);
             Console.WriteLine($"Winner:  Sum of unmarked numbers: {summeFrei} -> final score = {lastNumber * summeFrei}");
+        }
+        foreach (BingoBoard winner in finalBoards) {
+            int summeFrei = winner.GetUnmarked().Aggregate((sum, n) => n + sum);
+            Console.WriteLine($"Last winner:  Sum of unmarked numbers: {summeFrei} -> final score = {finalNumber * summeFrei}");
         }
     }
 
@@ -73,7 +80,9 @@ public class BingoSubsystem {
         if (Setup(out numberlist, out bblist, new StringReader(exampleinput))) {
             Run(numberlist, bblist);
         }
-        Console.WriteLine("--- Aufgabe 1: Find the winning board ---");
+        Console.WriteLine();
+
+        Console.WriteLine("--- Aufgabe 1/2: Find the firts/last winning board ---");
         if (Setup(out numberlist, out bblist, new StringReader(File.ReadAllText("04-bingo-subsys-input.txt")))) {
             Run(numberlist, bblist);
         }
@@ -81,17 +90,35 @@ public class BingoSubsystem {
 
     /// <summary>Play a Bingo round</summary>
     /// <returns>the list winning boards, the number drawn last is stored in winningNumber</return>
-    public static List<BingoBoard> Play(List<BingoBoard> boards, List<int> numberSequence, out int winningNumber) {
+    public static List<BingoBoard> Play(List<BingoBoard> boards, List<int> numberSequence,
+        out int winningNumber, out int lastWinningNumber, out List<BingoBoard> lastFinishers) {
+
         List<BingoBoard> winners = new List<BingoBoard>();
         winningNumber = -1;
+        lastWinningNumber = -1;
+        lastFinishers = new List<BingoBoard>();
+        int numFinished = 0;
         foreach (int n in numberSequence) {
             Console.WriteLine($"Next number: {n}");
+            List<BingoBoard> finishers = new List<BingoBoard>();   // Finishers for this round
             foreach (BingoBoard b in boards) {
-                if (b.nextNumber(n))  winners.Add(b);
+                if (b.IsFinished) continue;   // Skip testing already finished boards
+                if (b.nextNumber(n)) {
+                    finishers.Add(b);
+                    numFinished++;
+                    if (winningNumber < 0) {
+                        winners.Add(b);
+                    }
+                }
             }
-            if (winners.Count > 0) {
+            if (winners.Count > 0 && winningNumber < 0) {
                 Console.WriteLine("We have one or more winners!");
                 winningNumber = n;
+            }
+            if (numFinished >= boards.Count) {
+                Console.WriteLine("Everyone has finished their board now!");
+                lastFinishers = finishers;
+                lastWinningNumber = n;
                 break;
             }
         }
@@ -104,10 +131,12 @@ public class BingoBoard {
     public readonly int Columns = 5;
     private int[,] board;
     private bool[,] marked;
+    public bool IsFinished { get; private set; }
 
     public BingoBoard(System.IO.StringReader reader) {
         board = new int[5,5];
         marked = new bool[5,5];
+        IsFinished = false;
         int elementsread = 0;
         for (int i = 0; i < 5; i++) {
             string? line = reader.ReadLine();
@@ -141,8 +170,14 @@ public class BingoBoard {
     public bool isBingo() {
         // Test all 5 lines and columns
         for (int i = 0; i < 5; i++) {
-            if (marked[i,0] && marked[i,1] && marked[i,2] && marked[i,3] && marked[i,4]) return true;
-            if (marked[0,i] && marked[1,i] && marked[2,i] && marked[3,i] && marked[4,i]) return true;
+            if (marked[i,0] && marked[i,1] && marked[i,2] && marked[i,3] && marked[i,4]) {
+                IsFinished = true;
+                return true;
+            }
+            if (marked[0,i] && marked[1,i] && marked[2,i] && marked[3,i] && marked[4,i]) {
+                IsFinished = true;
+                return true;
+            }
         }
         return false;
     }
