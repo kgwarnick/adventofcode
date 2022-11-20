c21c97327a15f207d9963d82e3fc8b8feaa3fc94
kgwarnick <118844595+kgwarnick@users.noreply.github.com>
2022-11-20 15:22:44 +0100

    AoC 2015 Day 8, Teil 2 Escaping
    Ausgabe identisch von mono 6.12.0.122 und dotnet 6.0.402
    unter Gentoo Linux

--- 2015/08-matchsticks.cs
+++ 2015/08-matchsticks.cs
@@ -14,8 +14,25 @@ public class MatchsticksEscaping {
 
 
     public static string Escape (string valueString) {
-        return valueString.Replace ("\\", "\\\\")
-            .Replace ("\"", "\\\"");
+        System.Text.StringBuilder target =
+            new System.Text.StringBuilder (valueString.Length * 4 / 3);
+        // Add opening quote
+        target.Append ('\"');
+        // Copy characters to the target string, escaping where necessary
+        for (int i = 0; i < valueString.Length; i++) {
+            if (valueString[i] == '\\') {
+              target.Append ("\\\\");
+            }
+            else if (valueString[i] == '\"') {
+              target.Append ("\\\"");
+            }
+            else {
+              target.Append (valueString[i]);
+            }
+        }
+        // Add closing quote and return the built string
+        target.Append ('\"');
+        return target.ToString ();
     }
 
     public static string Unescape (string codeString) {
@@ -31,26 +48,20 @@ public class MatchsticksEscaping {
         while (i < temp.Length) {
           // System.Console.WriteLine ($"index: {i}, character {temp[i]}");
           if (temp[i] != '\\') {
-            target.Append (temp[i]);
-            i++;
-            continue;
+            target.Append (temp[i]);  i++;  continue;
           }
           if (i + 1 < temp.Length && temp[i+1] == '\\') {
-            target.Append ('\\');
-            i += 2;
-            continue;
+            // Escape backslash
+            target.Append ('\\');  i += 2;  continue;
           }
           if (i + 1 < temp.Length && temp[i+1] == '\"') {
-            target.Append ('\"');
-            i += 2;
-            continue;
+            // Escape double quote character
+            target.Append ('\"');  i += 2;  continue;
           }
           if (temp.Length > i + 3 && temp[i+1] == 'x') {
             // Replace ASCII characters given numerically as \xNN
             byte charCode = System.Convert.ToByte (temp.Substring (i + 2, 2), 16);
-            target.Append ((char)charCode);
-            i += 4;
-            continue;
+            target.Append ((char)charCode);  i += 4;  continue;
           }
           System.Console.WriteLine (
               $"Unsupported escape sequence: {temp[i+1]}");
@@ -59,12 +70,25 @@ public class MatchsticksEscaping {
         return target.ToString();
     }
 
-    public static int RunTest1 (string[] strlist, int logLevel = 1) {
+    public static int RunEscapeTest (string[] strlist, int logLevel = 1) {
+        int valueLen = 0, codeLen = 0;
+        foreach (string s in strlist) {
+            string e = Escape (s);
+            valueLen += s.Length;  codeLen += e.Length;
+            if (logLevel >= 2)  System.Console.WriteLine (
+                $"escape: {s} ({s.Length}) -> {e} ({e.Length})");
+        }
+        if (logLevel >= 1)  System.Console.WriteLine (
+            $"code length {codeLen} -> value length {valueLen}");
+        return codeLen - valueLen;
+    }
+
+    public static int RunUnescapeTest (string[] strlist, int logLevel = 1) {
         int valueLen = 0, codeLen = 0;
         foreach (string s in strlist) {
             string u = Unescape (s);
             codeLen += s.Length;  valueLen += u.Length;
-            System.Console.WriteLine (
+            if (logLevel >= 2)  System.Console.WriteLine (
                 $"unescape: {s} ({s.Length}) -> {u} ({u.Length})");
         }
         if (logLevel >= 1)  System.Console.WriteLine (
@@ -74,14 +98,14 @@ public class MatchsticksEscaping {
 
     public static void Main () {
 
-        System.Console.WriteLine ("--- Example ---");
-        int result = RunTest1 (ProvidedExamples);
+        System.Console.WriteLine ("--- Example Unescaping ---");
+        int result = RunUnescapeTest (ProvidedExamples, 2);
         System.Console.WriteLine (
             $"-> Difference in value and code length: {result}");
         System.Console.WriteLine ();
 
         System.Console.WriteLine ("--- Weitere Beispiele ---");
-        result = RunTest1 (MoreExamples);
+        result = RunUnescapeTest (MoreExamples, 2);
         System.Console.WriteLine (
             $"-> Difference in value and code length: {result}");
         System.Console.WriteLine ();
@@ -89,7 +113,25 @@ public class MatchsticksEscaping {
         System.Console.WriteLine ("--- Aufgabe 1: Escaping overhead ---");
         string[] inputLines = System.IO.File.ReadAllLines (
             "08-matchsticks-input.txt");
-        result = RunTest1 (inputLines);
+        result = RunUnescapeTest (inputLines, 1);
+        System.Console.WriteLine (
+            $"-> Difference in value and code length: {result}");
+        System.Console.WriteLine ();
+
+        System.Console.WriteLine ("--- Example Escaping ---");
+        result = RunEscapeTest (ProvidedExamples, 2);
+        System.Console.WriteLine (
+            $"-> Difference in value and code length: {result}");
+        System.Console.WriteLine ();
+
+        System.Console.WriteLine ("--- Weitere Beispiele ---");
+        result = RunEscapeTest (MoreExamples, 2);
+        System.Console.WriteLine (
+            $"-> Difference in value and code length: {result}");
+        System.Console.WriteLine ();
+
+        System.Console.WriteLine ("--- Aufgabe 2: Escaping overhead ---");
+        result = RunEscapeTest (inputLines);
         System.Console.WriteLine (
             $"-> Difference in value and code length: {result}");
         System.Console.WriteLine ();
