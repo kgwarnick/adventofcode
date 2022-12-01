478d8e23332d58be60639a6243359fd38bbfd5db
kgwarnick <118844595+kgwarnick@users.noreply.github.com>
2022-12-01 19:31:10 +0100

    AoC 2022 Day 1 Part 2 Top-Three Calorie Carriers

--- 2022/01-calorie-counting.cpp
+++ 2022/01-calorie-counting.cpp
@@ -5,6 +5,7 @@
 #include "fileread.hpp"
 
 #include <list>
+#include <map>
 #include <string>
 #include <iostream>
 
@@ -33,17 +34,27 @@ list<list<string> > LineGroups (list<string> lines) {
 }
 
 
-/// \brief Find line group with the greatest sum
+/// \brief Find line groups with the greatest sum
 //
-unsigned long int GetLargestCalorieCarrier (list<string> lines) {
+map <int, unsigned long int> GetTopThreeCalorieCarriers (list<string> lines) {
   list<list<string> > groups = LineGroups (lines);
-  unsigned long int maxcalories = 0;
+  map <int, unsigned long int> topcarriers;
+  topcarriers[1] = 0;  topcarriers[1] = 0;  topcarriers[2] = 0;
   for (list<string> gr : groups) {
     unsigned long int calories = 0;
     for (string s : gr)  calories += atol (s.c_str());
-    if (calories > maxcalories)  maxcalories = calories;
+    if (calories > topcarriers[1]) {
+      topcarriers[3] = topcarriers[2];
+      topcarriers[2] = topcarriers[1];
+      topcarriers[1] = calories;
+    } else if (calories > topcarriers[2]) {
+      topcarriers[3] = topcarriers[2];
+      topcarriers[2] = calories;
+    } else if (calories > topcarriers[3]) {
+      topcarriers[3] = calories;
+    }
   }
-  return maxcalories;
+  return topcarriers;
 }
 
 
@@ -53,11 +64,16 @@ int main () {
     "7000\n8000\n9000\n\n10000\n";
   list<string> examplelines = SplitString (exampleinput, "\n");
   cout << "Largest sum of calories in one group: "
-       << GetLargestCalorieCarrier (examplelines) << endl << endl;
+       << GetTopThreeCalorieCarriers (examplelines) [1] << ", "
+       << GetTopThreeCalorieCarriers (examplelines) [2] << ", "
+       << GetTopThreeCalorieCarriers (examplelines) [3] << endl << endl;
 
-  cout << "--- Puzzle 1: Largest calorie carrier ---" << endl;
+  cout << "--- Puzzle 1/2: Three largest calorie carriers ---" << endl;
   examplelines = ReadLines ("01-calorie-counting-input.txt");
+  map <int, unsigned long int> carriers =
+    GetTopThreeCalorieCarriers (examplelines);
   cout << "*** Largest sum of calories in one group: "
-       << GetLargestCalorieCarrier (examplelines) << " ***" << endl << endl;
+       << carriers[1] << " + " << carriers[2] << " + " << carriers[3] << " = "
+       << carriers[1] + carriers[2] + carriers[3] << " ***" << endl << endl;
   return 0;
 }
