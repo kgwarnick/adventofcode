beac93e440a7f7fd101b4acf3c32df2a0db63c1c
kgwarnick <118844595+kgwarnick@users.noreply.github.com>
2022-12-04 13:19:00 +0100

    AoC 2022 Day 4 Camp Cleanup in C

---
+++ 2022/04-camp-cleanup.c
@@ -0,0 +1,83 @@
+// Advent of Code 2022 Day 4: Camp Cleanup
+// https://adventofcode.com/2022/day/4
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "freadlns.h"
+
+
+/// \brief read section start and end points for two ranges "start1-end1,start2-end2"
+/// \return true on success, false on error
+//
+bool parsesections (const char *line, int *start1, int *end1, int *start2, int *end2) {
+  char *next;
+  // Read start of first section
+  *start1 = (int) strtoul (line, &next, 10);
+  // Skip hyphen -
+  if (*next == '-') next++; else return false;
+  // Read end of first section
+  *end1 = (int) strtoul (next, &next, 10);
+  // Skip comma ,
+  if (*next == ',') next++; else return false;
+  // Read start of second section
+  *start2 = (int) strtoul (next, &next, 10);
+  // Skip hyphen -
+  if (*next == '-') next++; else return false;
+  // Read end of second section
+  *end2 = (int) strtoul (next, &next, 10);
+  // Everything read
+  return true;
+}
+
+
+/// \brief Check whether one section if fully contained in the other
+//
+bool IsOneSectionContainedInTheOther (int start1, int end1, int start2, int end2) {
+  return ((start1 >= start2 && end1 <= end2) || (start2 >= start1 && end2 <= end1));
+}
+
+
+/// \brief Count in how many range pairs one is fully contained in the other
+//
+int NumberOfFullyContainedRanges (int numlines, char **lines, int loglevel) {
+  int numcontained = 0;
+  for (int i = 0; i < numlines; i++) {
+    int a1, a2, b1, b2;
+    bool parsesuccess = parsesections (lines[i], &a1, &a2, &b1, &b2);
+    bool onecontained = IsOneSectionContainedInTheOther (a1, a2, b1, b2);
+    if (onecontained) numcontained++;
+    if (loglevel >= 1)
+      printf ("%3d  %s  ->  %s  %3d ... %3d ,  %3d ... %d  %s\n", i, lines[i],
+        parsesuccess ? " PARSEOK  " : "PARSEERROR", a1, a2, b1, b2,
+        onecontained ? "[CONT]" : "[----]");
+  }
+  return numcontained;
+}
+
+
+int main () {
+
+  printf ("--- Examples ---\n");
+  char *examples[] = {
+    "2-4,6-8",  "2-3,4-5",  "5-7,7-9",  "2-8,3-7",  "6-6,4-6",  "2-6,4-8"
+  };
+  int numcontained = NumberOfFullyContainedRanges (
+    sizeof (examples) / sizeof (examples[0]), examples, 1);
+  printf ("Number of assignment pairs with "
+    "one range fully contained in the other: %d\n", numcontained);
+  printf ("\n");
+
+  printf ("--- Puzzle 1: Fully contained ranges ---\n"); 
+  size_t maxlines = 1001;
+  size_t numlines = 0;
+  char **lines = (char**) calloc (maxlines, sizeof (char*));
+  ssize_t numchars = readlines ("04-camp-cleanup-input.txt", &maxlines, &numlines, &lines);
+  printf ("Lines read: %zu (%zu), characters: %zd\n", numlines, maxlines, numchars);
+  numcontained = NumberOfFullyContainedRanges (
+    numlines, lines, 0);
+  printf ("Number of assignment pairs with "
+    "one range fully contained in the other: %d\n", numcontained);
+
+  return 0;
+}
---
+++ 2022/freadlns.h
@@ -0,0 +1,6 @@
+/// \file
+/// \brief File reading
+
+#include <stdio.h>
+
+ssize_t readlines (const char *filename, size_t *n, size_t *numlines, char ***lines);
