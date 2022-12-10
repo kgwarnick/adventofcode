97c785b13384a948b340f31c5745b7912b6d0fd2
kgwarnick <118844595+kgwarnick@users.noreply.github.com>
2022-12-10 11:27:57 +0100

    AoC 2022 Day 10 Part 2 Render characters

--- 2022/10-cathode-ray-tube.c
+++ 2022/10-cathode-ray-tube.c
@@ -8,13 +8,29 @@
 
 #include "freadlns.h"
 
+/// Character to use for a pixel switched on
+#define PIXELON_CHAR '#'
+/// Character to use for a pixel switched off
+#define PIXELOFF_CHAR '.'
 
 const char *shortexample[] = { "noop", "addx 3", "addx -5" };
 
 
-/// \brief Run the program and return the sum of signal strengths
+/// \brief Output ("render") the screen contents in text form
 //
-long int RunProgram (size_t numlines, const char **lines, int loglevel) {
+void OutputScreen (const char screen[6][40]) {
+  for (int l = 0; l < 6; l++)  printf ("%*.*s\n", 40, 40, screen[l]);
+}
+
+
+/// \brief Run the program and return the sum of signal strengths
+/// \param numlines  Number of instructions to run
+/// \param lines     Instructions to run
+/// \param screen    Pixels on a 6 lines x 40 column grid
+/// \param loglevel  Verbosity of output
+//
+long int RunProgram (size_t numlines, const char **lines,
+    char screen[][40], int loglevel) {
   long int cycle = 0;   // cycle counter
   long int regx = 1;    // register x value
   long int sigstrength = 0;   // sum of signal strengths
@@ -24,10 +40,10 @@ long int RunProgram (size_t numlines, const char **lines, int loglevel) {
     // Read instruction
     if (strncmp ("addx ", lines[i], 5) == 0) {
       waittime = 2;
-      long incr = strtol (lines[i] + 5, NULL, 10);
+      long incrx = strtol (lines[i] + 5, NULL, 10);
       if (loglevel >= 2)
-        printf ("%4ld  addx %3ld  %4ld\n", cycle + 1, incr, regx);
-      pendingx = regx + strtol (lines[i] + 5, NULL, 10);
+        printf ("%4ld  addx %3ld  %4ld\n", cycle + 1, incrx, regx);
+      pendingx = regx + incrx;
     }
     else if (strncmp ("noop", lines[i], 4) == 0) {
       if (loglevel >= 2)
@@ -36,13 +52,20 @@ long int RunProgram (size_t numlines, const char **lines, int loglevel) {
     // Run instruction
     // - wait necessary cycles
     for (int t = 0; t < waittime; t++) {
-      // Take samples during cycles 20, 60, 100, 140, ...
+      // Take signal strength samples during cycles 20, 60, 100, 140, ...
       if ((cycle + 20) % 40 == 39) {
         if (loglevel >= 1)
           printf ("%4ld   -->      %4ld  --> %ld\n", cycle + 1, regx,
             (cycle + 1) * regx);
         sigstrength += (cycle + 1) * regx;
       }
+      // Draw a pixel
+      // printf ("Store pixel: %d %d\n", cycle / 40, cycle % 40);
+      screen[cycle / 40 % 6][cycle % 40] =
+        (regx >= (cycle % 40) - 1 && regx <= (cycle % 40) + 1) ?
+          PIXELON_CHAR : PIXELOFF_CHAR;
+      if (loglevel >= 2 && cycle % 240 == 239)  OutputScreen (screen);
+      // Move on to next cycle
       cycle++;
     }
     // - set new register value
@@ -54,25 +77,39 @@ long int RunProgram (size_t numlines, const char **lines, int loglevel) {
 }
 
 
-int main () {
-  printf ("--- Example ---\n");
+/// \brief Run a program from file,
+///   return the sum of signal strengths and the screen content
+//
+long int RunFile (const char *filename, char screen[6][40], int loglevel) {
   size_t maxlines = 10;
   char **inputlines = (char**) calloc (maxlines, sizeof (char*));
   size_t numlines;
-  ssize_t numchars = readlines ("10-cathode-ray-tube-example.txt",
-    &maxlines, &numlines, &inputlines);
-  printf ("Lines read: %zd, characters: %zu\n", numlines, numchars);
-  long int sigstrsum = RunProgram (numlines, (const char**)inputlines, 1);
-  printf ("Signal strength sum: %ld\n", sigstrsum);
+  ssize_t numchars = readlines (filename, &maxlines, &numlines, &inputlines);
+  if (loglevel >= 1)
+    printf ("Lines read: %zd, characters: %zu\n", numlines, numchars);
+  long int sigstrsum = RunProgram (numlines, (const char**)inputlines,
+    screen, loglevel);
   for (int i = 0; i < numlines; i++)  free (inputlines[i]);
+  free (inputlines);
+  return sigstrsum;
+}
+
+
+int main () {
+  printf ("--- Example ---\n");
+  char screen[6][40];
+  long int sigstrsum = RunFile ("10-cathode-ray-tube-example.txt",
+    screen, 1);
+  printf ("Signal strength sum: %ld\n", sigstrsum);
+  OutputScreen (screen);
   printf ("\n");
 
   printf ("--- Puzzle 1: Sum of signal strengths ---\n");
-  numchars = readlines ("10-cathode-ray-tube-input.txt",
-    &maxlines, &numlines, &inputlines);
-  printf ("Lines read: %zd, characters: %zu\n", numlines, numchars);
-  sigstrsum = RunProgram (numlines, (const char**)inputlines, 1);
+  sigstrsum = RunFile ("10-cathode-ray-tube-input.txt", screen, 1);
   printf ("Signal strength sum: %ld\n", sigstrsum);
-  for (int i = 0; i < numlines; i++)  free (inputlines[i]);
-  free (inputlines);
+  printf ("\n");
+
+  printf ("--- Puzzle 2: Render characters on screen ---\n");
+  OutputScreen (screen);
+  return 0;
 }
--- 2022/freadlns.h
+++ 2022/freadlns.h
@@ -3,4 +3,21 @@
 
 #include <stdio.h>
 
-ssize_t readlines (const char *filename, size_t *n, size_t *numlines, char ***lines);
+/// \brief Read all lines from the file and store them in the char* array pointed to by lines
+///
+/// The array can be a pre-allocated array of size *maxlines, and will be reallocated as
+/// necessary to contain all lines.  The actual number of lines read is stored in *numlines.
+///
+/// The array itself can/should be allocated on calling this function,
+/// but no lines in the array should be allocated.  getline() will be called to read
+/// each line, allocating memory as necessary.
+/// After use, all the lines read (indicated by *numlines) as well as the array itself
+/// should be deallocated by the caller.
+///
+/// \param filename  File name to read
+/// \param maxlines  Pointer to the array size
+/// \param numlines  Pointer to the number of lines read
+/// \param lines     Pointer to char** to store the lines read from the file
+/// \return The number of characters read or -1 on failure
+//
+ssize_t readlines (const char *filename, size_t *maxlines, size_t *numlines, char ***lines);
