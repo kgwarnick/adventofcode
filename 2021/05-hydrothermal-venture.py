0ff59b41f405f4e1effcb8aa61029b7bcd5bbf66
kgwarnick <118844595+kgwarnick@users.noreply.github.com>
2022-12-14 15:02:04 +0100

    AoC 2021 Day 5 Visualisation with matplotlib

--- 2021/05-hydrothermal-venture.py
+++ 2021/05-hydrothermal-venture.py
@@ -1,6 +1,9 @@
 # Advent of Code 2021, Day 5 Hydrothermal Venture
 # https://adventofcode.com/2021/day/5
 
+import matplotlib.pyplot
+import numpy
+
 exampleinput = """0,9 -> 5,9
 8,0 -> 0,8
 9,4 -> 3,4
@@ -28,13 +31,17 @@ def isdiag (seg) -> bool:
     # for diagonals the difference in x is equal to +/- the difference in y -- UNTESTED
     return (seg[0][0] - seg[1][0] == seg[0][1] - seg[1][1]) or (seg[0][0] - seg[1][0] == - (seg[0][1] - seg[1][1]))
 
+def lineval (x, xstart, xend, ystart, yend):
+    """y-Wert für x-Wert auf einer Gerade von (xstart, ystart) nach (xend, yend) berechnen"""
+    return (yend - ystart) * (x - xstart) // (xend - xstart) + ystart
+
 def pointsx (lineseg: tuple[tuple[int,int], tuple[int,int]]) -> list[tuple[int,int]]:
     """Punktliste entlang x-Richtung"""
     if lineseg[0][0] < lineseg[1][0]:
         startpt = 0; endpt = 1
     else:
         startpt = 1; endpt = 0
-    return [ ( x, (lineseg[endpt][1] - lineseg[startpt][1]) * (x - lineseg[startpt][0]) // (lineseg[endpt][0] - lineseg[startpt][0]) + lineseg[startpt][1])
+    return [ (x, lineval (x, lineseg[startpt][0], lineseg[endpt][0], lineseg[startpt][1], lineseg[endpt][1]))
         for x in range (lineseg[startpt][0], lineseg[endpt][0] + 1) ]
 
 def pointsy (lineseg: tuple[tuple[int,int], tuple[int,int]]) -> list[tuple[int,int]]:
@@ -43,11 +50,14 @@ def pointsy (lineseg: tuple[tuple[int,int], tuple[int,int]]) -> list[tuple[int,i
         startpt = 0; endpt = 1
     else:
         startpt = 1; endpt = 0
-    return [ ( (lineseg[endpt][0] - lineseg[startpt][0]) * (y - lineseg[startpt][1]) // (lineseg[endpt][1] - lineseg[startpt][1]) + lineseg[startpt][0], y )
+    return [ (lineval (y, lineseg[startpt][1], lineseg[endpt][1], lineseg[startpt][0], lineseg[endpt][0]), y )
         for y in range (lineseg[startpt][1], lineseg[endpt][1] + 1) ]
 
 
 def ventpointshvd (linesegments: list[tuple[tuple[int, int], tuple[int,int]]], withdiag: bool = False) -> dict[tuple[int,int], int]:
+    """Dictionary erzeugen mit Zuordnung, wie viele Vents über jeden Punkt verlaufen:  (x, y) -> n
+    
+    Nur Punkte mit mindestens einem Vent sind aufgeführt"""
     d = dict()
     for lseg in linesegments:
         if ishoriz (lseg):
@@ -69,9 +79,39 @@ def ventpointshvd (linesegments: list[tuple[tuple[int, int], tuple[int,int]]], w
     return d
 
 
+def erzeugeventkarte (ventdict: dict[tuple[int,int], int], width: int = None, height: int = None) -> list[list[int]]:
+    """Liste von Listen mit Werten zu jedem Punkt, auch Null-Werten, erzeugen aus Dictionary ohne Null-Werten"""
+    # keine Breite oder Höhe angegeben, selbst bestimmen als größte x- oder y-Koordinate
+    if (width == None):
+        width = max (map (lambda punkt: punkt[0], ventdict))
+    if (height == None):
+        height = max (map (lambda punkt: punkt[1], ventdict))
+    # Liste von Listen mit Anzahl erzeugen
+    ventmap = list ()
+    for y in range (height + 1):
+        ventline = [ ventdict.get ((x, y), 0) for x in range (width + 1) ]
+        ventmap.append (ventline)
+    return ventmap
+
+def zeichneventkarte (ventdict: dict[tuple[int,int], int], plottext = True, plotgraphics = False, width: int = None, height: int = None) -> None:
+    ventmap = erzeugeventkarte (ventdict, width, height)
+    # Karte in Textform ausgeben
+    if plottext:
+        for y in range (len (ventmap)):
+            print (f"{y:3d}|", end="")
+            # Zeichen wählen für Anzahl:  '1' ... '9' oder '.' für 0, '*' für 10 oder mehr
+            ventstr = "".join (map (lambda v: str (v) if (v > 0 and v < 10) else '*' if v >= 10 else ".", ventmap[y]))
+            print (ventstr, "|", sep="")
+    # Karte mit matplotlib darstellen
+    if (plotgraphics):
+        fig, ax = matplotlib.pyplot.subplots ()
+        ax.imshow (ventmap)
+        matplotlib.pyplot.show ()
+
+
 if __name__ == "__main__":
     print ("--- Beispiel ---")
-    loglevel = 3
+    loglevel = 2
     parsedlines = parselines (exampleinput)
     print (parsedlines)
     points = ventpointshvd (parsedlines)
@@ -81,9 +121,10 @@ if __name__ == "__main__":
     print ()
     pointswithdiag = ventpointshvd (parsedlines, True)
     print (f"Points including diagonal vents: {pointswithdiag}, number: {len (pointswithdiag)}")
-    atleast2 = [ v for v in points if points[v] > 1 ]
+    atleast2 = [ v for v in pointswithdiag if pointswithdiag[v] > 1 ]
     print (f"Dangerous points (at least 2 vents): {atleast2}, number: {len (atleast2)}")
     print ()
+    zeichneventkarte (pointswithdiag, plottext=True, plotgraphics=True)
 
     print ("--- Aufgabe 1: Find dangerous points from horizontal and vertical vents ---")
     loglevel = 1
@@ -99,3 +140,4 @@ if __name__ == "__main__":
     print (f"With diagonal vents: {len (pointswithdiag)}")
     atleast2 = [ v for v in pointswithdiag if pointswithdiag[v] > 1 ]
     print (f"Dangerous points (at least 2 vents): {len (atleast2)}")
+    zeichneventkarte (pointswithdiag, plottext=False, plotgraphics=True)
