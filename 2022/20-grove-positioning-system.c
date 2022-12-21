7b5d8bffafe2e2aa813a195dc06043053060ad39
kgwarnick <118844595+kgwarnick@users.noreply.github.com>
2022-12-21 12:54:42 +0100

    AoC 2022 Day 20 Part 2 Decryption key and 10 mixes

--- 2022/20-grove-positioning-system.c
+++ 2022/20-grove-positioning-system.c
@@ -9,25 +9,23 @@
 /// \brief Element data type in a simple doubly-linked list
 //
 typedef struct Number_st {
-  int value;
+  long int value;
   struct Number_st *prev;
   struct Number_st *next;
 } Number;
 
 
-/// \brief Do one or more mixes of the numbers on the input lines
+/// \brief Do one or more mixes of the numbers on the input lines, multiplying each number with the key first
 //
-long RunMixer (size_t numlines, const char **lines, int nummix) {
+long RunMixer (size_t numlines, const char **lines, int nummix, int multkey) {
   Number numbers[numlines];
   for (size_t i = 0; i < numlines; i++) {
-    numbers[i].value = strtol (lines[i], NULL, 10);
+    numbers[i].value = strtol (lines[i], NULL, 10) * multkey;
     numbers[i].prev = &(numbers[i > 0 ? i - 1 : numlines - 1]);
     numbers[i].next = &(numbers[i < numlines - 1 ? i + 1 : 0]);
   }
   for (int nmix = 0; nmix < nummix; nmix++) {
     for (size_t i = 0; i < numlines; i++) {
-      // printf ("%4zu  %4d <- %4d -> %4d\n", i, numbers[i].prev->value,
-      //   numbers[i].value, numbers[i].next->value);
       Number *thisnum = &(numbers[i]);
       Number *newpos = &(numbers[i]);
       // Move forward to new position
@@ -40,9 +38,6 @@ long RunMixer (size_t numlines, const char **lines, int nummix) {
         for (int j = 0; j > numbers[i].value % (int)(numlines - 1); j--)
           newpos = newpos->prev;
       // Adapt neighbour pointers: take out old position, insert new
-      // printf ("thisnum: %d -> prev %d, next %d;  newpos: %d -> %d, %d\n",
-      //   thisnum->value, thisnum->prev->value, thisnum->next->value,
-      //   newpos->value, newpos->prev->value, newpos->next->value);
       if (numbers[i].value % (int)(numlines - 1) != 0) {
         Number *oldprev = thisnum->prev, *oldnext = thisnum->next;
         thisnum->prev->next = oldnext;   // oldprev --++++++++--oldnext
@@ -52,14 +47,7 @@ long RunMixer (size_t numlines, const char **lines, int nummix) {
         newpos->prev->next = thisnum;    // newprev --> [ thisnum ] <-- newpos
         newpos->prev = thisnum;
       }
-      // printf ("thisnum: %d -> prev %d, next %d;  newpos: %d -> %d, %d\n",
-      //   thisnum->value, thisnum->prev->value, thisnum->next->value,
-      //   newpos->value, newpos->prev->value, newpos->next->value);
       // Output new order
-      // for (size_t j = 0; j < numlines; j++) {
-      //   printf ("%4zu  %4d <- %4d -> %4d\n", j, numbers[j].prev->value,
-      //     numbers[j].value, numbers[j].next->value);
-      // }
       // - Forward (to check next pointers)
       // Number *n = &(numbers[0]);
       // for (size_t j = 0; j <= numlines; j++, n = n->next) {
@@ -87,7 +75,7 @@ long RunMixer (size_t numlines, const char **lines, int nummix) {
   for (int count = 0; count < 3; count++) {
     for (size_t skip = 0; skip < 1000 % numlines; skip++)
       currnum = currnum->next;
-    printf ("Adding number: %d\n", currnum->value);
+    printf ("Adding number: %ld\n", currnum->value);
     result += currnum->value;
   }
   return result;
@@ -95,11 +83,17 @@ long RunMixer (size_t numlines, const char **lines, int nummix) {
 
 
 const char *examplelines[] = { "1", "2", "-3", "3", "-2", "0", "4" };
+const long decryptkey = 811589153;
 
 int main () {
   printf ("--- Example ---\n");
   long result = RunMixer (sizeof (examplelines) / sizeof (examplelines[0]),
-    examplelines, 1);
+    examplelines, 1, 1);
+  printf ("* Result: %ld *\n", result);
+  printf ("\n");
+  printf ("--- Example with key ---\n");
+  result = RunMixer (sizeof (examplelines) / sizeof (examplelines[0]),
+    examplelines, 10, decryptkey);
   printf ("* Result: %ld *\n", result);
   printf ("\n");
 
@@ -109,7 +103,12 @@ int main () {
   /* ssize_t numchars = */ readlines ("20-grove-positioning-system-input.txt",
     &maxlines, &numlines, &inputlines);
   // printf ("Read %zu lines, %zd characters\n", numlines, numchars);
-  result = RunMixer (numlines, (const char**)inputlines, 1);
+  result = RunMixer (numlines, (const char**)inputlines, 1, 1);
+  printf ("*** Result: %ld ***\n", result);
+  printf ("\n");
+
+  printf ("--- Puzzle 2: Mix ten times and use decryption key ---\n");
+  result = RunMixer (numlines, (const char**)inputlines, 10, decryptkey);
   printf ("*** Result: %ld ***\n", result);
   return 0;
 }
